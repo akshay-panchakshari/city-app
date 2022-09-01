95df57ee6473e34206952bcd83677284af12d463
Akshay <panchakshari.akshay@gmail.com>
2022-09-01 11:13:41 +0530

    Initial commit

---
+++ src/test/java/com/city/app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.city.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
