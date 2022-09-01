a832ea2a2cdfe79038d01de6e3746e0849ff9b33
Akshay <panchakshari.akshay@gmail.com>
2022-09-01 12:47:54 +0530

    DB and security setup

---
+++ src/main/java/com/city/app/model/City.java
@@ -0,0 +1,43 @@
+package com.city.app.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class City {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String id;
+	private String name;
+	private String photo;
+	public City(){
+		
+	}
+	public City(String id, String name, String photo) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.photo = photo;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+}
